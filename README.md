8c0e3320737bc75198416760f3a3156c910eb4fb
joshuaciencia <joshuaciencia2016@gmail.com>
2019-11-08 17:02:13 +0000

    task 4

---
+++ README.md
@@ -0,0 +1,4 @@
+0x00. AirBnB clone - The console
+By:
+	Yennifer Marcelita Tobon Yate
+	Joshua Hernandez Martinez
--- models/base_model.py
+++ models/base_model.py
@@ -4,12 +4,15 @@ from uuid import uuid4
 from datetime import datetime
 
 class BaseModel:
-    def __init__(self, *argv, **kwargs):
-        self.id = str(uuid4())
-        self.created_at = datetime.today()
-        self.updated_at = datetime.today()
-        self.argv = argv
-	self.kwargs = kwargs
+    def __init__(self, *args, **kwargs):
+        if len(kwargs) is not 0:
+            self.updated_at = datetime.strptime(kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+            self.created_at = datetime.strptime(kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+            self.id = kwargs["id"]
+        else:
+            self.updated_at = datetime.today()
+            self.created_at = datetime.today()
+            self.id = str(uuid4())
 
     def __str__(self):
         return "[{}] ({}) {}".format(BaseModel.__name__, self.id, self.__dict__)
