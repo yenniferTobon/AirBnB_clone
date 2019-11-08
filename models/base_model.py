258f1148bd46804b76b7f46fbe59b01aad813fbf
yenniferTobon <ymtobony@unal.edu.co>
2019-11-08 15:57:52 +0000

    excerise 3

--- models/base_model.py
+++ models/base_model.py
@@ -1,29 +1,27 @@
 #!/usr/bin/python3
 
-import uuid
-import datetime
+from uuid import uuid4
+from datetime import datetime
 
 class BaseModel:
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.today()
-        self.updated_at = datetime.datetime.today()
+    def __init__(self, *argv, **kwargs):
+        self.id = str(uuid4())
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
+        self.argv = argv
+	self.kwargs = kwargs
 
     def __str__(self):
         return "[{}] ({}) {}".format(BaseModel.__name__, self.id, self.__dict__)
 
     def save(self):
-        self.updated_at = datetime.datetime.today()
+        self.updated_at = datetime.today()
 
     def to_dict(self):
-        new_dict = {}
+        new_dict = self.__dict__.copy()
         new_dict["__class__"] = BaseModel.__name__
-        for key,value in self.__dict__.items():
-            v = value
-            if key == "created_at" or key == "updated_at":
-                v = v.isoformat()
-            new_dict[key] = v
+        new_dict["created_at"] = new_dict["created_at"].isoformat()
+        new_dict["updated_at"] = new_dict["updated_at"].isoformat()
         return new_dict
-
      
             
