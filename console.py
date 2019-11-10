3fcbb72d5f17f2739039be942bffb519bc78ac64
yenniferTobon <ymtobony@unal.edu.co>
2019-11-10 22:46:33 +0000

    console.py with function create and show

--- console.py
+++ console.py
@@ -2,6 +2,8 @@
 
 import sys
 import cmd
+from models.base_model import BaseModel
+import models
 
 class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
@@ -20,6 +22,38 @@ class HBNBCommand(cmd.Cmd):
 
     def emptyline(self):
         pass
+
+    def do_create(self, argv):
+        if argv == "BaseModel":
+            new_instance = BaseModel()
+            models.storage.new(new_instance) 
+            models.storage.save()
+            print("{}".format(new_instance.id))
+        elif len(argv) is 0:
+            print("** class name missing **")
+        elif argv is not "BaseModel":
+            print("** class doesn't exist **")
+
+    def help_create(self):
+        print("create instances")
+
+    def do_show(self, argv):
+        argument_split = argv.split()
+        aux = 0
+       
+        if len(argument_split) == 0:
+            print("** class name missing **")
+        elif argument_split[0] != "BaseModel":
+            print("** class doesn't exist **")
+        elif len(argument_split) < 2:
+            print("** instance id missing **")
+        elif argument_split[0] == "BaseModel":
+            for key, obj in models.storage.all().items():
+                if key == argument_split[0]+"."+argument_split[1]:
+                    aux = 1
+                    print(obj)   
+            if aux == 0:
+                print("** no instance found **")
 
 if __name__ == "__main__": 
     
