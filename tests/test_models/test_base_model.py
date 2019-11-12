7c3f473c2cb970d711a66604df8c30c7c1fed629
yenniferTobon <ymtobony@unal.edu.co>
2019-11-12 22:12:05 +0000

    test_base_model.py add test pep8

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -3,7 +3,9 @@
 import unittest
 from models.base_model import BaseModel
 from datetime import datetime
+import pep8
 import uuid
+import models
 
 class Test_BaseModel(unittest.TestCase):
 
@@ -16,22 +18,28 @@ class Test_BaseModel(unittest.TestCase):
     def test_Id_Is_String(self):
         self.assertEqual(type(self.obj.id), str)
 
-   # def test_doctrins(self):
-   #     self.assertIsNotNone(BaseModel.__doc__)
+    def test_doctrins(self):
+        self.assertIsNotNone(BaseModel.__doc__)
 
-   # def test_pep8_base_model(self):
-   #     """ Test for PEP8 ok. """
-   #     pep8style = pep8.StyleGuide(quiet=True)
-   #     result = pep8style.check_files(['models/base_model.py'])
-   #     self.assertEqual(result.total_errors, 0, "Please fix pep8")
+    def test_pep8_base_model(self):
+        """ Test for PEP8 """
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['models/base_model.py'])
+        self.assertEqual(result.total_errors, 0, "Please fix pep8")
 
     def test_id_uuid_v4(self):
         """Test version 4 of UUID"""
         test__version = uuid.UUID(self.obj.id).version
         self.assertEqual(test__version, 4, "Error: Different version")
 
-   # def test_created_at_Is_datatime(self):
-   #     self.assertIs(self.obj_BM.created_at, datetime.datetime)
+    def test_created_at_Is_datatime(self):
+        self.assertIsInstance(self.obj.created_at, datetime)
+
+    def test_save(self):
+        self.obj.save()
+        key = self.obj.__class__.__name__+"."+self.obj.id
+        self.assertEqual(self.obj, models.storage.all()[key]) 
+        
 
 if __name__ == "__main__":
     unittest.main()
