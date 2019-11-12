b7c02d7f840f22847d1f4480aa62ea192d557f54
yenniferTobon <ymtobony@unal.edu.co>
2019-11-12 22:58:43 +0000

    test_base_model.py pep8

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-
+"""Module to make tests"""
 import unittest
 from models.base_model import BaseModel
 from datetime import datetime
@@ -7,46 +7,64 @@ import pep8
 import uuid
 import models
 
+
 class Test_BaseModel(unittest.TestCase):
+    """ Class to test
+    """
 
     def setUp(self):
-        self.obj = BaseModel() 
+        """Function that execute before of each test function
+        """
+        self.obj = BaseModel()
 
     def test_createBaseModel(self):
+        """Test that is instance
+        """
         self.assertIsInstance(self.obj, BaseModel)
 
     def test_Id_Is_String(self):
+        """Id is string
+        """
         self.assertEqual(type(self.obj.id), str)
 
-    def test_doctrins(self):
+    def test_docString(self):
+        """Test if function and class have docString
+        """
         self.assertIsNotNone(BaseModel.__doc__)
 
     def test_pep8_base_model(self):
-        """ Test for PEP8 """
+        """ Test for PEP8
+        """
         pep8style = pep8.StyleGuide(quiet=True)
         result = pep8style.check_files(['models/base_model.py'])
         self.assertEqual(result.total_errors, 0, "Please fix pep8")
 
     def test_id_uuid_v4(self):
-        """Test version 4 of UUID"""
+        """Test version 4 of UUID
+        """
         test__version = uuid.UUID(self.obj.id).version
         self.assertEqual(test__version, 4, "Error: Different version")
 
     def test_created_at_Is_datatime(self):
+        """Test that created_at is instance of datetime
+        """
         self.assertIsInstance(self.obj.created_at, datetime)
 
     def test_save(self):
+        """Test save objects in a json file
+        """
         self.obj.save()
         key = self.obj.__class__.__name__+"."+self.obj.id
         self.assertEqual(self.obj, models.storage.all()[key])
 
     def test_to_dict(self):
+        """Test to compare to two dictonary
+        """
         new_dict = self.obj.__dict__.copy()
         new_dict["__class__"] = self.obj.__class__.__name__
         new_dict["created_at"] = new_dict["created_at"].isoformat()
         new_dict["updated_at"] = new_dict["updated_at"].isoformat()
-        self.assertDictEqual(new_dict, self.obj.to_dict())  
-        
+        self.assertDictEqual(new_dict, self.obj.to_dict())
 
 if __name__ == "__main__":
     unittest.main()
